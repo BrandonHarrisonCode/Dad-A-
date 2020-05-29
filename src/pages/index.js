bca76b0a2c12a0f3629cdb23a760e584c98c533a
Brandon Harrison <brandonharrisoncode@gmail.com>
2020-05-29 11:33:44 -0600

    Switch to gatsbyjs

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Home() {
+  return (
+  <div>
+    <h1>The Dad-A-Base</h1>
+    <p><subtitle>The best collection of dad jokes on the web.</subtitle></p>
+    </div>
+  );
+}
