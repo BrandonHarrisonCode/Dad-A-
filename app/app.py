ed433a36cd8dde35b7e9a77d587ddee896932ca9
Brandon Harrison <brandonharrisoncode@gmail.com>
2020-05-30 10:19:56 -0600

    Remove frontend and add random joke stub

--- app/app.py
+++ app/app.py
@@ -1,9 +1,17 @@
 from flask import Flask
-app = Flask(__name__, static_folder='../frontend/build', static_url_path='/')
 
-@app.route('/')
+app = Flask(__name__)
+
+
+@app.route("/")
 def index():
-    return app.send_static_file('index.html')
+    return "This is the Dad-A-Base api."
+
+
+@app.route("/random")
+def random():
+    return "Random joke"
+
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 80))
+    app.run(host="0.0.0.0", debug=False, port=os.environ.get("PORT", 80))
